967f4350c82fa40ccc828cec89aac4f59da82c61
Jeisson Gomez <71471533+jeypitbull@users.noreply.github.com>
2022-11-17 22:34:49 -0500

    Actualizando del proyecto

--- index.php
+++ index.php
@@ -6,12 +6,22 @@ use\Slim\App;
 
 $app = new App();
 
-$app->get("/pruebas", function() use($app){
+$db = new mysqli('localhost', 'root', ' ', 'curso_angular4');
+
+$app->get("/pruebas", function() use($app, $db){
     echo "Hola Mundo desde el Slim de PHP";
 });
 
 $app->get("/probando", function() use($app){
     echo "Otro texto Cualquiera";
+});
+
+$app->post('/productos', function() use($app, $db){
+    $json = $request->getBody()->getContents();
+    $data = json_decode($json, true);
+    
+    var_dump($json);
+    var_dump($data);
 });
 
 $app->run();
